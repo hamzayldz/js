9d548bd612481ce4334f7c62c44c317344bafa36
hamzayldz <hamzayldz@yandex.com>
2022-01-26 01:39:40 +0300

    link projesi

---
+++ link kısaltıcı/script.js
@@ -0,0 +1,30 @@
+// Information to reach API
+const apiKey = '9cd71aeaf35a4844bc26108eaefefd3a';
+const url = 'https://api.rebrandly.com/v1/links';
+
+const inputField = document.getElementById("input")
+
+const shortenButton = document.getElementById("shorten")
+const shortenUrl = () => {
+    const urlToShorten = inputField.value
+    console.log(urlToShorten)
+    const data = JSON.stringify({ destination: urlToShorten });
+    console.log(data)
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            renderResponse(xhr.response);
+        }
+    }
+    xhr.open("POST", url)
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.setRequestHeader('apikey', apiKey)
+
+    xhr.send(data);
+    
+}
+
+// page elements
+
+shortenButton.addEventListener("click", displayShortUrl)
